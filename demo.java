123a06d42a1c0d9d8280160ce0b659dd541906dd
bthimmareddy <bthimmareddyb2@gmail.com>
2018-08-13 08:09:46 +0000

    bank trans

--- demo.java
+++ demo.java
@@ -1,6 +1,26 @@
-class demo
+class bank trans
+{
+   p void main()
+    {
+      ........
+       s.o.p("hello");
+    }
+}
 {
    p.s void main()
+    {
+      ........
+       s.o.p("hello");
+    }
+}
+{
+   p.s void main()
+    {
+      ........
+       s.o.p("hello");
+    }
+}
+p.s void main()
     {
       ........
        s.o.p("hello");
